beb398036114f322f2345090e72a89c68d30e850
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-08 18:48:48 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.a4626708cf4773e80ca7e08d29cb9862.js"
 );
 
 self.addEventListener('message', (event) => {
