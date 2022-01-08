ce14cbba46744446222e18f02cea42710240620b
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-08 05:07:30 -0500

    update App.js with new configuration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import {
-  navBar,
-  mainBody,
-  about,
-  repos,
-  leadership,
-  skills,
-  getInTouch,
-  experiences
+    navBar,
+    mainBody,
+    about,
+    repos,
+    skills,
+    getInTouch,
+    experiences,
+    career,
+    education,
+    Publications
 } from "./editable-stuff/config.js";
 import MainBody from "./components/home/MainBody";
 import AboutMe from "./components/home/AboutMe";
@@ -16,85 +18,90 @@ import Project from "./components/home/Project";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Skills from "./components/home/Skills";
-// import { Blog } from "./components/blog/Blog";
-// import BlogPost from "./components/blog/BlogPost";
 import GetInTouch from "./components/home/GetInTouch.jsx";
-import Leadership from "./components/home/Leadership.jsx";
 
 import Experience from "./components/home/Experience";
+import Career from "./components/home/Career";
+import Education from "./components/home/Education";
+import Publication from "./components/home/Publication";
 
 const Home = React.forwardRef((props, ref) => {
-  return (
-    <>
-      <MainBody
-        gradient={mainBody.gradientColors}
-        title={`${mainBody.firstName} ${mainBody.middleName} ${mainBody.lastName}`}
-        message={mainBody.message}
-        icons={mainBody.icons}
-        ref={ref}
-      />
-      {about.show && (
-        <AboutMe
-          heading={about.heading}
-          message={about.message}
-          link={about.imageLink}
-          imgSize={about.imageSize}
-          resume={about.resume}
-        />
-      )}
-      {
-        experiences.show && (
-          <Experience experiences={experiences}/>
-        )
-      }
-      {repos.show && (
-        <Project
-          heading={repos.heading}
-          username={repos.gitHubUsername}
-          length={repos.reposLength}
-          specfic={repos.specificRepos}
-        />
-      )}
-      {leadership.show && (
-        <Leadership
-          heading={leadership.heading}
-          message={leadership.message}
-          img={leadership.images}
-          imageSize={leadership.imageSize}
-        />
-      )}
-      {skills.show && (
-        <Skills
-          heading={skills.heading}
-          hardSkills={skills.hardSkills}
-          softSkills={skills.softSkills}
-        />
-      )}
-      
-    </>
-  );
+    return (
+        <>
+            <MainBody
+                gradient={mainBody.gradientColors}
+                title={`${mainBody.firstName} ${mainBody.middleName} ${mainBody.lastName}`}
+                message={mainBody.message}
+                icons={mainBody.icons}
+                ref={ref}
+            />
+            {about.show && (
+                <AboutMe
+                    heading={about.heading}
+                    message={about.message}
+                    link={about.imageLink}
+                    imgSize={about.imageSize}
+                    resume={about.resume}
+                />
+            )}
+            {career.show && (
+                <Career
+                    heading={career.heading}
+                    careerList={career.lists}
+                />
+            )}
+            {education.show && (
+                <Education
+                    heading={education.heading}
+                    careerList={education.lists}
+                />
+            )}
+            {repos.show && (
+                <Project
+                    heading={repos.heading}
+                    username={repos.gitHubUsername}
+                    length={repos.reposLength}
+                    specfic={repos.specificRepos}
+                />
+            )}
+            {skills.show && (
+                <Skills
+                    heading={skills.heading}
+                    hardSkills={skills.hardSkills}
+                    softSkills={skills.softSkills}
+                />
+            )}
+            {experiences.show && (
+                <Experience experiences={experiences}/>
+            )}
+            {Publications.show && (
+                <Publication
+                    heading={Publications.heading}
+                    list={Publications.list}
+                />
+            )}
+        </>
+    );
 });
 
 const App = () => {
-  const titleRef = React.useRef();
+    const titleRef = React.useRef();
 
-  return (
-    <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
-      {navBar.show && <Navbar ref={titleRef} />}
-      <Route path="/" exact component={() => <Home ref={titleRef} />} />
-      {/* {false && <Route path="/blog" exact component={Blog} />}
-      {false && <Route path="/blog/:id" component={BlogPost} />} */}
-      <Footer>
-        {getInTouch.show && (
-          <GetInTouch
-            heading={getInTouch.heading}
-            message={getInTouch.message}
-            email={getInTouch.email}
-          />
-        )}
-      </Footer>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+            {navBar.show && <Navbar ref={titleRef}/>}
+            <Route path="/" exact component={() => <Home ref={titleRef}/>}/>
+            <Footer>
+                {getInTouch.show && (
+                    <GetInTouch
+                        heading={getInTouch.heading}
+                        message={getInTouch.message}
+                        email={getInTouch.email}
+                    />
+                )}
+            </Footer>
+        </BrowserRouter>
+    );
 };
 
 export default App;
