c430e94a43346cc77fe08c5559a36c9fcbb40763
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-08 18:52:02 -0500

    Updates

---
+++ precache-manifest.a4626708cf4773e80ca7e08d29cb9862.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185ae04ade2eace8d028515425422039",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "500f4d6bd4750ad74916",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "44cf6b21abfcfb47c34e",
+    "url": "/home/static/js/2.c51de536.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.c51de536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500f4d6bd4750ad74916",
+    "url": "/home/static/js/main.e987034e.chunk.js"
+  },
+  {
+    "revision": "8e294fbec71a811f2893",
+    "url": "/home/static/js/runtime-main.afad27ed.js"
+  },
+  {
+    "revision": "b0fe76bcfdc072b6e3647aafa6881694",
+    "url": "/home/static/media/resume.b0fe76bc.pdf"
+  },
+  {
+    "revision": "c7295f46b169e56a2c2fc24e87a3ac86",
+    "url": "/home/static/media/wasim.c7295f46.png"
+  }
+]);
