e53bbb49a02f0be870a629d69fd7aad8e22dd1fb
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-08 05:08:33 -0500

    added new sections to the configuration

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -1,155 +1,141 @@
-// Navigation Bar SECTION
 const navBar = {
-  show: true,
+    show: true,
 };
 
-// Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
-  middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
-  icons: [
-    {
-      image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
-    },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
-    {
-      image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
-    },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
-  ],
+    gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+    firstName: "Wasim",
+    middleName: "",
+    lastName: "Boughattas",
+    message: "Java R Python • SQL Flux Spark • InfluxDB PowerBI • Docker Ubuntu",
+    icons: [{
+        image: "fa-github", url: "https://github.com/wboughattas",
+    }, {
+        image: "fa-linkedin", url: "https://www.linkedin.com/in/wasim-boughattas-335849165/",
+    }]
 };
-
-// ABOUT SECTION
-// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
-//a) your Instagram username
-//      i.e:profilePictureLink:"johnDoe123",
-//b) a link to an hosted image
-//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
-//c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
-//d) If you do not want any picture to be displayed, just leave it empty :)
-//      i.e: profilePictureLink: "",
-// For Resume either provide link to your resume or import from "editable-stuff" directory
-//     i.e resume: require("../editable-stuff/resume.pdf"),
-//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 
 const about = {
-  show: true,
-  heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
-  imageSize: 375,
-  message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    show: true,
+    heading: "About Me",
+    imageLink: require("./wasim.png"),
+    imageSize: 375,
+    message: "I am a final-year undergraduate at Concordia University and pursuing a degree in actuarial math and " + "computer science. I am most passionate about data engineering, machine learning, and predictive analysis. " + "I have broad knowledge in many fields including: data engineering, machine learning, " + "artificial intelligence, risk models and probabilistic methods.",
+    resume: require("./resume.pdf"),
 };
 
-// PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
-//      i.e: reposLength: 0,
-// If you want to display specfic projects, add the repository names,
-//      i.e ["repository-1", "repo-2"]
+const career = {
+    show: true, heading: "Career", lists: [{
+        CompanyName: "Deloitte Canada",
+        Address: "Quebec, MTL",
+        Title: "Omnia AI: Data Engineer - Co-op/Intern",
+        Time: "January, 2021 – April, 2021",
+        Descriptions: [
+            ["▪ Created a generalized solution to Big Data case requests by generalizing an existing Python" +
+            "code from a previous specific case. Worked on Ubuntu Server and employ Apache tools (Hadoop, Spark, hive) "],
+            ["▪ Worked with business managers to understand business objectives and develop automated reporting " +
+            "solutions using DAX, and M in Power BI "],
+            ["▪ Created automated python processes that enable data manipulation using Pandas and display the console" +
+            "into an end-user-friendly GUI application using the Gooey library"]],
+        TechStack: ["Python, Spark, HiveSQL, Linux, PowerBI"]
+    }, {
+        CompanyName: "Deloitte Canada",
+        Address: "Quebec, MTL",
+        Title: "Omnia AI: Data Engineer - Co-op/Intern",
+        Time: "May, 2022 – August, 2022",
+        Descriptions: [],
+        TechStack: [""]
+    }]
+};
+
+const education = {
+    show: true, heading: "Education", lists: [{
+        Name: "Concordia University",
+        Address: "Quebec, MTL",
+        Title: "BSc, Specialization in Actuarial Mathematics (co-op)",
+        Time: "2019 - Expected 2022",
+        Descriptions: [
+            ["▪ Member of the Golden Key Society"],
+            ["▪ Irshad Family Scholarship (Entrance Bursary in Actuarial Mathematics)"],
+            ["▪ Dean’s Roll for 3.75+ GPA (2020)"]],
+    }, {
+        Name: "Concordia University",
+        Address: "Quebec, MTL",
+        Title: "Minor, Computer Science",
+        Time: "2019 - Expected 2022",
+        Descriptions: [],
+    }]
+};
+
 const repos = {
-  show: true,
-  heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
-  specificRepos: [],
+    show: true,
+    heading: "Personal Projects",
+    gitHubUsername: "wboughattas", //i.e."johnDoe12Gh"
+    reposLength: 0,
+    specificRepos: ['Stock-Trading-Robot', 'COVID-19-Vaccination-System', 'Electricity-Price-Forecasting'],
 };
 
-// Leadership SECTION
-const leadership = {
-  show: false,
-  heading: "Leadership",
-  message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-  images: [
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-  ],
-  imageSize: {
-    width:"615",
-    height:"450"
-  }
-};
-
-// SKILLS SECTION
 const skills = {
-  show: true,
-  heading: "Skills",
-  hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
-  ],
-  softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
-  ],
+    show: true,
+    heading: "Skills",
+    hardSkills: [
+        {name: "Python", value: 80},
+        {name: "R", value: 80},
+        {name: "SQL", value: 80},
+        {name: "Flux", value: 70},
+        {name: "Java", value: 70},
+        {name: "Bash", value: 65},
+        {name: "Docker", value: 65},
+        {name: "Spark", value: 65}
+    ],
+    softSkills: [
+        {name: "Data Analysis", value: 80},
+        {name: "Machine Learning", value: 75},
+        {name: "Data Engineering", value: 70},
+        {name: "Probabilistic Methods", value: 60},
+        {name: "Actuarial Risk Models", value: 60},
+        {name: "Natural Language Processing", value: 55},
+        {name: "Artificial Intelligence", value: 50},
+        {name: "Multi-Agent Reinforcement Learning", value: 50}
+    ]
 };
 
-// GET IN TOUCH SECTION
+const Publications = {
+    show: true, heading: "Publications", list: [{
+        title: "The Science Behind Losing Fat",
+        url: "https://wasimboga.wordpress.com/2022/01/08/the-science-behind-losing-fat/",
+        description:
+            "There are lots of contradictory diet theories that when it comes to losing fat makes you want to dive " +
+            "into that chocolate sundae. Losing weight isn't easy and doing it in a sustainable way can make the " +
+            "task feel difficult. While there isn’t an easy fix to losing weight, there are plenty of steps you can " +
+            "take to develop a healthier relationship with food."
+    }]
+}
+
 const getInTouch = {
-  show: true,
-  heading: "Get In Touch",
-  message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    show: true,
+    heading: "Get In Touch",
+    message: "I'm currently looking for a 3rd internship that involves data engineering, machine learning, " + "and predictive analysis! If you know of any positions available, if you have any questions, or if you just " + "want to say hi, please feel free to email me at",
+    email: "wboughattas@outlook.com",
 };
 
 const experiences = {
-  show: false,
-  heading: "Experiences",
-  data: [
-    {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
-    },
-    {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
-    },
-  ]
+    show: false, heading: "Experiences", data: [{
+        role: 'Software Engineer', company_logo: require('../assets/img/dell.png'), date: 'June 2018 – Present',
+    }, {
+        role: 'Front-End Developer', company_logo: require('../assets/img/boeing.png'), date: 'May 2017 – May 2018',
+    }]
 }
 
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
-
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export {
+    navBar,
+    mainBody,
+    about,
+    repos,
+    skills,
+    getInTouch,
+    experiences,
+    career,
+    education,
+    Publications
+};
