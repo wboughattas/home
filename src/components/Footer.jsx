29677af7ba7df6452e35f3837bfcc43ae97c18a0
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-08 05:10:17 -0500

    updated the original footer with newer hyperlinks and messages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,31 +2,43 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 
 const Footer = (props) => {
-  const bgStyle = { backgroundColor: "#f5f5f5" };
+    const bgStyle = {backgroundColor: "#f5f5f5"};
 
-  return (
-    <footer style={bgStyle} className="mt-auto py-5 text-center ">
-      <Container>
-        {props.children}
-        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
-        <a
-          className="badge badge-dark"
-          rel="noopener"
-          href="https://github.com/hashirshoaeb"
-          aria-label="My GitHub"
-        >
-          Hashir Shoaib
-        </a>{" "}
-        using <i className="fab fa-react" />
-        <p>
-          <small className="text-muted">
-            Project code is open source. Feel free to fork and make your own
-            version.
-          </small>
-        </p>
-      </Container>
-    </footer>
-  );
+    return (
+        <footer style={bgStyle} className="mt-auto py-5 text-center ">
+            <Container>
+                {props.children}
+
+                Code available <a
+                className="badge"
+                style={{color: "black"}}
+                rel="noopener"
+                target=" _blank"
+                href="https://github.com/wboughattas/home"
+                aria-label="here"
+            >
+                <i className="fab fa-github"/> Here
+            </a>
+                <br/>
+                Original code <i className="fas fa-code"/> by{" "}
+                <a
+                    className="badge badge-dark"
+                    rel="noopener"
+                    href="https://github.com/hashirshoaeb/home"
+                    aria-label="My GitHub"
+                >
+                    Hashir Shoaib
+                </a>{" "}
+
+                <p>
+                    <small className="text-muted">
+                        Project code is open source. Feel free to fork and make your own
+                        version.
+                    </small>
+                </p>
+            </Container>
+        </footer>
+    );
 };
 
 export default Footer;
