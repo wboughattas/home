5629ae2a38b022fc6eb022046fa49681452b9169
Wasim Boughattas <wassooma2013@yahoo.com>
2022-01-08 22:09:08 -0500

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,17 @@ const Footer = (props) => {
         <footer style={bgStyle} className="mt-auto py-5 text-center ">
             <Container>
                 {props.children}
+
+                Code available <a
+                className="badge"
+                style={{color: "black"}}
+                rel="noopener"
+                target=" _blank"
+                href="https://github.com/wboughattas/home"
+                aria-label="here"
+            >
+                <i className="fab fa-github"/> Here
+            </a>
             </Container>
         </footer>
     );
